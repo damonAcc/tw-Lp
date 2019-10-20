c7772124f59fe9ce9dbd31283b6fd323ca8dd3ad
damon <717474983@qq.com>
2019-10-21 00:36:16 +0800

    --

--- src/api/report.js
+++ src/api/report.js
@@ -1,7 +1,7 @@
 import { request } from './requset'
 
 let basePath = 'http://47.56.199.89:3000/'
-basePath = 'http://localhost:3000/'
+// basePath = 'http://localhost:3000/'
 
 export const reportSubmit = data => {
     return request({
